d8ab7ca91778b2ef998fe354198f5cf54ad4d334
Ashish <aashishvyass@gmail.com>
2018-01-30 11:05:21 +0530

    commit first

---
+++ src/test/java/team/project/firstTest.java
@@ -0,0 +1,12 @@
+package team.project;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class firstTest {
+
+	@Test
+	public void jenTest(){
+		Assert.assertEquals("dsd", "gfg");
+	}
+}
